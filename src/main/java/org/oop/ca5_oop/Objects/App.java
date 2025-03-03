6acf944e12e332b22422a79c4c7ecaf2a1692fc6
chealy90 <christopherhealy20@gmail.com>
2025-03-03 22:54:20 +0100

    Menu function created

--- src/main/java/org/oop/ca5_oop/Objects/App.java
+++ src/main/java/org/oop/ca5_oop/Objects/App.java
@@ -1,7 +1,40 @@
 package org.oop.ca5_oop.Objects;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+        app.start();
+    }
+
+    public void start(){
+        int choice = 0;
+        do {
+            choice = runMenu();
+            System.out.println("Chosen " + choice);
+        } while (choice != 7);
+    }
+
+    public int runMenu(){
+        Scanner kb = new Scanner(System.in);
+        System.out.println("---Menu---");
+        System.out.println("1) Get all products.");
+        System.out.println("2) Find product by key.");
+        System.out.println("3) Delete product by key.");
+        System.out.println("4) Create product.");
+        System.out.println("5) Update product.");
+        System.out.println("6) Filter by quantity in stock.");
+        System.out.print("--Your choice: ");
+        int choice;
+        choice = kb.nextInt();
+        kb.nextLine();
+        return choice;
+
+    }
 
 }
+
+
+
 
