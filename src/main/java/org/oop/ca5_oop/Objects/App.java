1550bc9e6ca5e82f78f83801de62886992b6445f
Vialsiu <D00265675@student.dkit.ie>
2025-03-05 11:10:19 +0100

    Start merge and test with christopher-menu and features 3,4

--- src/main/java/org/oop/ca5_oop/Objects/App.java
+++ src/main/java/org/oop/ca5_oop/Objects/App.java
@@ -13,18 +13,47 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         ProductDao productDao = new ProductDao();
+        App app = new App();
+        app.start();
+    }
+
+    public void start(){
+        int choice = 0;
+        do {
+            choice = runMenu();
+            System.out.println("Chosen " + choice);
+        } while (choice != 7);
+    }
+
+    public int runMenu(){
+        Scanner kb = new Scanner(System.in);
+        System.out.println("---Menu---");
+        System.out.println("1) Get all products.");
+        System.out.println("2) Find product by key.");
+        System.out.println("3) Delete product by key.");
+        System.out.println("4) Create product.");
+        System.out.println("5) Update product.");
+        System.out.println("6) Filter by quantity in stock.");
+        System.out.print("--Your choice: ");
+        int choice;
+        choice = kb.nextInt();
+        kb.nextLine();
+        return choice;
 
 
-        try {
-            List<Product> products = productDao.listAllProducts();
-            for (Product product : products) {
-                System.out.println(product);
-            }
-
-        } catch (DaoException e) {
-            System.out.println("Error retrieving products: " + e.getMessage());
-            e.printStackTrace();
-        }
+//
+//        try {
+//            List<Product> products = productDao.listAllProducts();
+//            for (Product product : products) {
+//                System.out.println(product);
+//            }
+//
+//        } catch (DaoException e) {
+//            System.out.println("Error retrieving products: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//
+//
 
 
     }
